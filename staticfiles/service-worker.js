3b30ee7a252487eb710f890ca4e830bda68cd591
bersin <bersinmail@gmail.com>
2025-08-26 10:05:43 +0530

    Ready for deployment

---
+++ staticfiles/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "loanapp-cache-v1";
+const urlsToCache = [
+  "/",
+  "/static/manifest.json",
+  "/static/icons/icon-192x192.png",
+  "/static/icons/icon-512x512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
